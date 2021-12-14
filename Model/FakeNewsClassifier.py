b69e85eda4393487389dc16f3a3f3e05667efcce
matsant01 <94721367+matsant01@users.noreply.github.com>
2021-12-14 23:07:15 +0100

    Add files via upload

--- Model/FakeNewsClassifier.py
+++ Model/FakeNewsClassifier.py
@@ -28,63 +28,19 @@ import numpy as np
 
 
 df=pd.read_csv('train.csv') #import dataset
-
-
-# In[3]:
-
-
 df.head()
-
-
-# In[4]:
-
-
 df=df.dropna() #drop missing values (nan values)
 x=df.drop('label',axis=1) #drop also the label
 y=df['label'] #the label that tells me wheter the news is fake or not will be the output
-
-
-# In[5]:
-
-
 print("Tensorflow -> ",tf.__version__) # 2.7.0
 print("Python -> ",python_version())   # 3.8.12
 
 
-# In[6]:
-
-
 voc_size=5000 #vocabulary size
-
-
-# ## Data cleaning
-
-# In[7]:
-
-
 messages=x.copy()
 messages.reset_index(inplace=True)
-
-
-# In[8]:
-
-
-messages['title'][2]
-# messages['text'][3]
-
-
-# In[9]:
-
-
 nltk.download('stopwords')
-
-
-# ### Data preprocessing
-
-# In[10]:
-
-
-#stemming is the process of reducing words to their word stem, base or root form
+stemming is the process of reducing words to their word stem, base or root form
 ps = PorterStemmer()
 corpus = []
 for i in range(0, len(messages)):
@@ -97,72 +53,32 @@ for i in range(0, len(messages)):
     corpus.append(review)
 
 
-# In[11]:
-
-
-# through one hot every word is represented as a number that is specific for that single word
 onehot_repr=[one_hot(words,voc_size)for words in corpus]
-
-
-# ## Creating model
-
-# In[13]:
-
-
 sent_length=20
 embedded_docs=pad_sequences(onehot_repr,padding='pre',maxlen=sent_length) # fix sentences' lentgh
 dataset=np.array(embedded_docs)
 
-# In[14]:
 
-
-# the embedding layer converts the input into a vector with a specific number of features
-embedding_vector_features=50
+embedding_vector_features=64
 model = tf.keras.Sequential([
     tf.keras.layers.Embedding(voc_size,embedding_vector_features,input_length=sent_length),
     tf.keras.layers.GlobalMaxPooling1D(),
-    tf.keras.layers.Dense(24, activation='relu'),
-    tf.keras.layers.Dense(6, activation='softmax')
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
 ])
-#plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
-model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 model.summary()
 
 
-# In[15]:
-
-
 len(embedded_docs),y.shape
-
-
-# In[16]:
-
-
 x_final=np.array(embedded_docs)
 y_final=np.array(y)
 
 
-# In[17]:
-
-
-x_final.shape,y_final.shape
-
-
-# In[18]:
-
-
+#x_final.shape,y_final.shape
 x_train, x_test, y_train, y_test = train_test_split(x_final, y_final, test_size=0.33, random_state=42)
-
-
-# ### Model Training
-
-# In[19]:
-
-
 model.fit(x_train,y_train,validation_data=(x_test,y_test),epochs=10,verbose=2)
 
-
-# In[24]:
 
 
 model.save('float.h5')
@@ -182,23 +98,10 @@ print('------------------------------------\n')
 quantized_model.save('quantized_model.h5')
 print('Saved quantized model to')
 
-# ### Performance Metrics And Accuracy
 
-# In[21]:
-
-
-predict_x=model.predict(x_test)
+#predict_x=model.predict(x_test)
 #y_pred=(model.predict(x_test) > 0.5).astype("int32")
-y_pred=np.argmax(model.predict(x_test),axis=1)
-y_pred[0]
+#y_pred=np.argmax(model.predict(x_test),axis=1)
 
-
-# In[22]:
-
-
-confusion_matrix(y_test,y_pred)
-
-
-# In[23]:
-
-print(accuracy_score(y_test,y_pred))
+#confusion_matrix(y_test,y_pred)
+#print(accuracy_score(y_test,y_pred))
